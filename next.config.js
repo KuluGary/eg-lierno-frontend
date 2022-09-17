4b3ff959124bee5c1ac646be1f61c8d3ece9a803
Gary <kululu.gary@gmail.com>
2022-09-17 22:09:10 +0200

    removed some next configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
+  // webpackDevMiddleware: (config) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300,
+  //   };
 
-    return config;
-  },
+  //   return config;
+  // },
 };
